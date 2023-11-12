461500f785e6198a202fa8729b21b044411ce2af
Deep Kothari <deepk2891@gmail.com>
2023-11-12 22:38:50 +0530

    Quantity Selector step-1
    When generating HTML, add a unique class to <select> to identify which product the dropdown is for (class="js-quantity-selector-${ product.id }")

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -51,7 +51,7 @@ products.forEach((product) => {
             <div class="product-price">$${ (product.priceCents / 100).toFixed(2) }</div>
 
             <div class="product-quantity-container">
-                <select>
+                <select class="js-quantity-selector-${ product.id }">
                     <option selected value="1">1</option>
                     <option value="2">2</option>
                     <option value="3">3</option>
