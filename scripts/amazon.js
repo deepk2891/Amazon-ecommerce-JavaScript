9af888c9d9e6a86354f9fe2d9a33e5acc1b30675
Deep Kothari <deepk2891@gmail.com>
2023-11-12 23:18:46 +0530

    Quantity Selector step-3
    Get the value selected in the quantity selector (to get the value out of a <select> element, I used the property '.value')

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -97,6 +97,7 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
         })
 
         const quantitySelector = document.querySelector(`.js-quantity-selector-${ productId }`)
+        const quantity = quantitySelector.value
 
         if (matchingItem)
         {
