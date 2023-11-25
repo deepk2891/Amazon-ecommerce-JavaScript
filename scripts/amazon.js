3be010ba43135c4031eab38c7f98c187b3756242
Deep Kothari <deepk2891@gmail.com>
2023-11-25 12:58:06 +0530

    Use modules and import function addToCart from cart.js
    Group related code together into its own file.

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js"
+import { cart,addToCart } from "../data/cart.js"
 import { products } from "../data/products.js"
 
 let productsHTML = ''
@@ -53,32 +53,6 @@ products.forEach((product) => {
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML
 
-function addToCart (productId) {
-    let matchingItem
-    cart.forEach((item) => {
-        if (productId === item.productId)
-        {
-            matchingItem = item
-        }
-    })
-
-    const quantitySelector = document.querySelector(`.js-quantity-selector-${ productId }`)
-    const quantity = Number(quantitySelector.value)
-
-    if (matchingItem)
-    {
-        matchingItem.quantity += quantity
-    }
-    else
-    {
-        cart.push({
-            // productId: productId,
-            // quantity: quantity
-            productId,
-            quantity
-        })
-    }
-}
 
 function updateCartQuantity (productId) {
 
