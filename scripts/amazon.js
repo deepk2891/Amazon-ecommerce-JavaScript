f3344fe30979c84259e4983fba04523fbcfb1377
Deep Kothari <deepk2891@gmail.com>
2023-11-25 12:42:06 +0530

    Create Function addToCart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -53,37 +53,39 @@ products.forEach((product) => {
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML
 
+function addToCart (productId) {
+    let matchingItem
+    cart.forEach((item) => {
+        if (productId === item.productId)
+        {
+            matchingItem = item
+        }
+    })
+
+    const quantitySelector = document.querySelector(`.js-quantity-selector-${ productId }`)
+    const quantity = Number(quantitySelector.value)
+
+    if (matchingItem)
+    {
+        matchingItem.quantity += quantity
+    }
+    else
+    {
+        cart.push({
+            // productId: productId,
+            // quantity: quantity
+            productId,
+            quantity
+        })
+    }
+}
+
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     button.addEventListener('click',() => {
         // const productId = button.dataset.productId
         const { productId } = button.dataset
 
-        let matchingItem
-
-        cart.forEach((item) => {
-            if (productId === item.productId)
-            {
-                matchingItem = item
-            }
-        })
-
-        const quantitySelector = document.querySelector(`.js-quantity-selector-${ productId }`)
-        const quantity = Number(quantitySelector.value)
-
-        if (matchingItem)
-        {
-            matchingItem.quantity += quantity
-        }
-        else
-        {
-            cart.push({
-                // productId: productId,
-                // quantity: quantity
-                productId,
-                quantity
-            })
-
-        }
+        addToCart(productId)
 
         let cartQuantity = 0
         cart.forEach((item) => {
